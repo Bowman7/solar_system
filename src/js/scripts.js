bf41b236e0436ce5d07e0da1c0a1c0ac5b9dd42f
Bowman7 <110030682+Bowman7@users.noreply.github.com>
2023-02-16 19:35:14 +0530

    Update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -98,67 +98,7 @@ const saturn= generatePlanet(10,saturnTexture,132,{innerRadius:10,outerRadius:20
 const uranus=generatePlanet(7,uranusTexture,176);
 const neptune=generatePlanet(7,neptuneTexture,200);
 const pluto=generatePlanet(2.8,plutoTexture,216);
-//function to generate all planets
-// function createPlanete(size,texture,position)
-// {
-//     const Geo=new THREE.SphereGeometry(size,30,30);
-//     const Material=new THREE.MeshStandardMaterial({
-//          map: textureLoader.load(texture)
-//     });
-//     const mesh=new THREE.Mesh(Geo,Material);
-//     const obj=new THREE.Object3D();
-//     obj.add(mesh);
-//     scene.add(obj); 
-//     mesh.position.x=position;
-//     return mesh
-// }
 
-//mercury= createPlanete(3.2,mercuryTexture,28);
-
-//mercury 
-// const mercuryGeo=new THREE.SphereGeometry(3.2,30,30);
-// const mercuryMaterial=new THREE.MeshStandardMaterial({
-//     map: textureLoader.load(mercuryTexture)
-// });
-// const mercury=new THREE.Mesh(mercuryGeo,mercuryMaterial);
-// // sun.add(mercury);
-// const mercuryObj= new THREE.Object3D();
-// mercuryObj.add(mercury);
-// scene.add(mercuryObj);
-// mercury.position.x=28;
-
-// //venus
-// const venusGeo=new THREE.SphereGeometry(5,30,30);
-// const venusMaterial=new THREE.MeshStandardMaterial({
-//     map: textureLoader.load(venusTexture)
-// });
-// const venus=new THREE.Mesh(venusGeo,venusMaterial);
-// const venusObj=new THREE.Object3D();
-// venusObj.add(venus);
-// scene.add(venusObj);
-// venus.position.x=32;
-
-// //saturn 
-// const saturnGeo=new THREE.SphereGeometry(10,30,30);
-// const saturnMaterial=new THREE.MeshStandardMaterial({
-//     map: textureLoader.load(saturnTexture)
-// });
-// const saturn=new THREE.Mesh(saturnGeo,saturnMaterial);
-// const saturnObj=new THREE.Object3D();   
-// saturnObj.add(saturn);
-// scene.add(saturnObj);
-// saturn.position.x=132;
-// //saturn ring
-// const saturnRingGeo =new THREE.RingGeometry(10,20,30);
-// const saturnRingMat=new THREE.MeshStandardMaterial({
-//     map: textureLoader.load(saturnRing),
-//     side: THREE.DoubleSide
-// });
-// const satRing=new THREE.Mesh(saturnRingGeo,saturnRingMat);
-// saturnObj.add(satRing);
-// satRing.position.x=132;
-// satRing.rotation.x=-0.5* Math.PI;
-//declaring a point light taking parameter as color ,intensity and the max distance it can reach
 const pointLight=new THREE.PointLight(0xFFFFFF,2,300);
 scene.add(pointLight);
 // // Sets a 12 by 12 gird helper
